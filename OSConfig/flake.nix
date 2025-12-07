f09e4172c15293fdda62be6ece47d0f089fb50ee
Emman-pip <parrenoemmanuel756@gmail.com>
2025-12-07 08:40:25 +0800

    Make a functional flakes.

--- OSConfig/flake.nix
+++ OSConfig/flake.nix
@@ -1,18 +1,19 @@
 {
-  description = "A very basic flake";
+  description = "My first flake system.";
 
   inputs = {
     # nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
     nixpkgs.url = "nixpkgs/nixos-25.11";
   };
-
-  outputs = { self, nixpkgs }: {	
-		modules = [
-				./configuration.nix
-		];
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-
-    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
-
-  };
+  outputs = { self, nixpkgs, ... }:
+	let 
+		lib = nixpkgs.lib;
+	in {
+		nixosConfigurations = {
+				nixos = lib.nixosSystem {
+					system = "x86_64-linux";
+					modules = [ ./configuration.nix ];
+				};
+		};
+	};
 }
