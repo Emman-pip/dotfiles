a454b2323fb20a91e30296bafcaf08df8dc7c583
Emman-pip <parrenoemmanuel756@gmail.com>
2025-12-22 12:15:03 +0800

    Add nix-ld for compatibility.

--- OSConfig/configuration.nix
+++ OSConfig/configuration.nix
@@ -214,6 +214,19 @@
     };
   };
 
+  programs.nix-ld.enable = true;
+  programs.nix-ld.libraries = with pkgs; [
+    stdenv.cc.cc
+    zlib
+    fuse3
+    icu
+    nss
+    openssl
+    curl
+    expat
+    # Add other common libraries if needed
+  ];
+
   hardware.bluetooth = {
     enable = true;
     powerOnBoot = false;
